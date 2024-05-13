d700548a12548b01f2a4d232a631991e57e9c457
unknown <gbemiemmanuel555@gmail.com>
2024-05-13 16:18:38 +0100

    components added and header done

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,18 @@ import "bootstrap/dist/css/bootstrap.min.css";
 // Bootstrap Bundle JS
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import Header from "./components/Header/Header";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./components/Home/Banner/Home";
 
 function App() {
   return (
     <>
+      <BrowserRouter>
       <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,43 +1,60 @@
-import React from 'react'
+import { NavLink } from "react-router-dom"
+import img from '../../assets/12782ac2b8ae20cf43f86afa1f4f6076.png'
+
 
 const Header = () => {
   return (
     <div>
-      <nav className="navbar sticky-top navbar-expand-lg bg-body-tertiary">
+      <nav className="navbar sticky-top navbar-expand-lg">
   <div className="container">
-    <a className="navbar-brand" href="#">Navbar</a>
+    <NavLink>
+     <img className="" src={img} alt="" width='30px'/>
+    </NavLink>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+      <ul className="navbar-nav mx-auto  mb-lg-0 nav_ul">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
+          <NavLink className="nav-link" to='/'>Home</NavLink>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Link</a>
-        </li>
-        <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul className="dropdown-menu">
-            <li><a className="dropdown-item" href="#">Action</a></li>
-            <li><a className="dropdown-item" href="#">Another action</a></li>
-            <li><a className="dropdown-item" href="#">Something else here</a></li>
-          </ul>
+          <NavLink className="nav-link" to='/About'>About</NavLink>
+        </li> 
+        <li className="nav-item"> 
+          <NavLink className="nav-link" to='/Products'>Products</NavLink>
         </li>
         <li className="nav-item">
-          <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+          <NavLink className="nav-link" to='/contact'>Contact Us</NavLink>
         </li>
       </ul>
       <form className="d-flex" role="search">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-        <button className="btn btn-outline-success" type="submit">Search</button>
+        <img 
+          src="https://cdn-icons-png.flaticon.com/512/1040/1040225.png"
+          width='26'
+          height='26'
+          alt=""
+          // data-bs-toggle="modal"
+          // data-bs-target="#exampleModel"
+          // onClick={() => setSearch(true)}
+          className="img-small mx-3" />
+
+          <NavLink to="">
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/4903/4903878.png"
+              width="26"
+              height="26"
+              alt=""
+              // data-bs-toggle="offcanvas"
+              // data-bs-target="#offcanvasRight"
+              // aria-controls="#offcanvasRight"
+              // onClick={() => setShowCart(true)}
+              className="img-small" /> 
+          </NavLink>
       </form>
     </div>
   </div>
-</nav>
+</nav> 
     </div>
   )
 }
